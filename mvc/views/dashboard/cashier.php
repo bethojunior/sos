fcd96ae48cbb6b2812369dc6b3dbc7437dbef759
bethojunior <hgomescostajr@gmail.com>
2019-06-18 20:33:35 -0300

    ok

--- mvc/views/dashboard/cashier.php
+++ mvc/views/dashboard/cashier.php
@@ -14,3 +14,21 @@
         <tbody id="mountCashier"></tbody>
     </table>
 </div>
+
+<div id="modal-products-sale" class="modal bottom-sheet">
+    <div class="modal-content">
+        <h4>Produtos vendidos nesta venda</h4>
+        <table class="striped highlight">
+            <thead>
+            <tr>
+                <th>Produtos</th>
+                <th>Valor</th>
+            </tr>
+            </thead>
+            <tbody id="mount-products-list-sale"></tbody>
+        </table>
+    </div>
+    <div class="modal-footer">
+        <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Fechar</a>
+    </div>
+</div>
--- webfiles/js/modulos/dashboard/cashier.js
+++ webfiles/js/modulos/dashboard/cashier.js
@@ -8,19 +8,20 @@ function initUsers() {
         ContentController.getCashier().then(response => {
             let list = '';
             list += response.data.map(res => {
-                if(res.type !== typeStudant){
                     return `
                     <tr class="thisUser" id="${res.id}">
                         <td>${res.name}</td>
-                        <td>${res.what}</td>
+                        <td class="open-products" id="${res.what}">${res.what}</td>
                         <td>${res.data_who}</td>
                         <td>R$ ${res.value_sale}</td>
                     </tr>
                `;
-                }
             }).join(' ');
-
             write.innerHTML = list;
+            elementProperty.addEventInElement('.open-products','onclick',function(){
+                let products = this.getAttribute('id');
+                openModalProducts(products);
+            });
         });
     });
 }
@@ -32,4 +33,36 @@ function getValueMonth() {
             here.innerHTML = 'R$ '+response.total;
         })
     })
+}
+
+function openModalProducts(products){
+    let product = products.split(',');
+    let that_ = [];
+    product.map(id => {
+        let data = {};
+        data.id = id;
+        ContentController.getProductById(data).then(callback => {
+           that_.push(callback.data[0])
+        });
+    });
+
+    setTimeout(()=>{
+        mountProductsSales(that_);
+    },500);
+}
+
+function mountProductsSales(products) {
+    $('#modal-products-sale').modal('open');
+    elementProperty.getElement('#mount-products-list-sale',table => {
+        let list = '';
+        list += products.map(res => {
+            return `
+                    <tr class="thisUser" id="${res.id}">
+                        <td>${res.name}</td>
+                        <td>R$ ${res.value}</td>
+                    </tr>
+               `;
+        }).join(' ');
+        table.innerHTML = list;
+    });
 }
